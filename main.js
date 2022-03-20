7f84af138401ad2f9b5b9a86e75b12892d6c2583
luckii-mxk <95324075+luckii-mxk@users.noreply.github.com>
2022-03-20 17:53:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function start() {
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/25rhzVdxN/model.json", modelReady);
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+    console.log(results)
+    }
+}
